024b52c5d366cd651fcaf874bf6bd243416f24cf
Yury Chuchvaha <gagael23@live.com>
2019-06-18 00:56:42 +0300

    feat: fully implement color selection functionality

--- piskel-clone/src/components/Main/ColorSelect/ColorSelect.jsx
+++ piskel-clone/src/components/Main/ColorSelect/ColorSelect.jsx
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import './ColorSelect.scss';
 
 class ColorSelect extends Component {
-  state = {
-    primaryColor: '#000000',
-    secondaryColor: '#ffffff',
+  constructor(props) {
+    super(props);
+    this.primary = React.createRef();
+    this.secondary = React.createRef();
+
+    this.state = {
+      primaryColor: '#000000',
+      secondaryColor: '#ffffff',
+    };
+  }
+
+  handlePrimaryChange = ({ target }) => {
+    if (target === this.primary.current) {
+      this.setState({
+        primaryColor: target.value,
+      });
+    } else {
+      this.setState({
+        secondaryColor: target.value,
+      });
+    }
   };
 
   render() {
@@ -13,14 +31,32 @@ class ColorSelect extends Component {
     return (
       <section className="color-select-section">
         <div className="colors">
-          <div
-            className="colors__primary"
-            style={{ background: primaryColor }}
-          />
-          <div
-            className="colors__secondary"
-            style={{ background: secondaryColor }}
-          />
+          <div>
+            <input
+              type="color"
+              className="colors__input-primary"
+              defaultValue={primaryColor}
+              ref={this.primary}
+              onInput={this.handlePrimaryChange}
+            />
+            <div
+              className="colors__primary"
+              style={{ backgroundColor: primaryColor }}
+            />
+          </div>
+          <div>
+            <input
+              type="color"
+              className="colors__input-secondary"
+              defaultValue={secondaryColor}
+              ref={this.secondary}
+              onInput={this.handlePrimaryChange}
+            />
+            <div
+              className="colors__secondary"
+              style={{ backgroundColor: secondaryColor }}
+            />
+          </div>
         </div>
       </section>
     );
